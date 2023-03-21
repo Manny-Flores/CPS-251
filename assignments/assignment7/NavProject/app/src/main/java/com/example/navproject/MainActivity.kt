2ad946b81ffa9867924ad9624a6919f56d2c3511
unknown <mmflores@wccnet.edu>
2023-03-20 21:23:04 -0700

    updates

---
+++ assignments/assignment7/NavProject/app/src/main/java/com/example/navproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.navproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ assignments/assignment7/NavProject/app/src/main/java/com/example/navproject/ui/main/MainFragment.kt
@@ -0,0 +1,64 @@
+package com.example.navproject.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navproject.R
+import com.example.navproject.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        //binding.img1.setOnClickListener {
+            //val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond("Image 1")
+            //action.setMessage(binding.userText.text.toString())
+            //Navigation.findNavController(it).navigate(action)
+        //}
+
+        binding.img1.setOnClickListener{
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 1")
+            Navigation.findNavController(it).navigate(action)
+        }
+        binding.img2.setOnClickListener{
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 2")
+            Navigation.findNavController(it).navigate(action)
+        }
+        binding.img3.setOnClickListener{
+            val action: MainFragmentDirections.MainToSecond  =  MainFragmentDirections.mainToSecond()
+            action.setMessage("Image 3")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
